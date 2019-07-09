3c4b6e5794b4bad715421aefa81747ccc9b98f03
tsune440 <tsune440@gmail.com>
2019-07-09 09:32:33 +0700

    Login Logout pulldown

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
@@ -7,5 +9,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup' , to:'users#new'
   get '/links' , to:'static_pages#links'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
