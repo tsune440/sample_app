e70b41b190b5346ca18b3b1b446835835813db31
tsune440 <tsune440@gmail.com>
2019-07-09 11:11:22 +0700

    Blank pass user update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :name, presence:true, length: { maximum: 50 }
     VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEXP }, uniqueness: { case_sensitive: true }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   has_secure_password
 end
