14030fd8fd55af73f440b56efc85fcc250dd7c33
tsune440 <tsune440@gmail.com>
2019-07-11 11:33:23 +0700

    Add Picture 1MB limited

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,7 @@ class MicropostsController < ApplicationController
         flash[:success] = "Micropost created!"
         redirect_to root_url
       else
+        @feed_items = []
         render 'static_pages/home'
       end
   end
@@ -19,7 +20,7 @@ class MicropostsController < ApplicationController
 
   private
   def micropost_params
-    params.require(:micropost).permit(:content)
+    params.require(:micropost).permit(:content, :picture)
   end
 
   def
